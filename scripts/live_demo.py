f656d76973b16f0319c2967bd2a0c26c05becf18
cinbarker <cinbarker@gmail.com>
2024-07-02 18:44:26 +0200

    update inference and demo scripts

--- scripts/live_demo.py
+++ scripts/live_demo.py
@@ -24,14 +24,17 @@ with mp.solutions.face_mesh.FaceMesh(refine_landmarks=True) as face_mesh:
             landmarks = ilg.landmarks_detection(frame, results, True, marker_size=3)
             landmarks = landmarks[:468]  # Omit the iris landmarks
             normalized_landmarks = ilg.normalize_landmarks(landmarks, ilg.reference_landmarks)
-            expression = infer_model(normalized_landmarks)
+            expression, hidden_embeddings = infer_model(normalized_landmarks, return_hidden_embeddings=True)
+            hidden_embeddings = hidden_embeddings[0].detach().numpy()  # Convert tensor to numpy array
         else:
             expression = 'None'
+            hidden_embeddings = []
 
         end_time = time.time() - start_time
         fps = frame_counter / end_time
 
         cv2.flip(frame, 1, frame)
+        # frame = utils.draw_embeddings(frame, hidden_embeddings)
         frame = utils.textWithBackground(frame, f'FPS: {round(fps, 1)} EXPRESSION: {expression}', cv2.FONT_HERSHEY_SIMPLEX, 1.0, (40, 40), bgOpacity=0.5, textThickness=2, corner='bottom-right')
         cv2.imshow('frame', frame)
 
